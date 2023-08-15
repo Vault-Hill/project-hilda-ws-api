3949e194fbfa0d7b5797472053cde4dd20a55388
Geeb <omowole.gbenga@gmail.com>
2023-08-15 09:44:28 +0100

    feat: integrate with model api on sagemaker

--- handlers/welcome.js
+++ handlers/welcome.js
@@ -9,7 +9,14 @@ module.exports.handler = async (event, context) => {
   // TODO: Create context in elastiCache using connectionId as key
 
   //  Send message to client
-  const response = { message: 'Welcome! My name is Hilda. How may I help you?' };
+  const response = {
+    action: 'connect',
+    data: {
+      role: 'Hilda',
+      message: 'Welcome! My name is Hilda. How may I help you?',
+      timestamp: Date.now(),
+    },
+  };
 
   await postToConnection(callbackUrl, connectionId, response);
 
