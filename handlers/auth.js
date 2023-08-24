5c9584a838b08d3ebf727b6edd2e0bf91c299db9
Geeb <omowole.gbenga@gmail.com>
2023-08-24 12:46:20 +0100

    feat: add dynamodb component

--- handlers/auth.js
+++ handlers/auth.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const { createRedisClient } = require('../helpers/createRedisClient');
+const { DynamoDBClient, PutItemCommand, GetItemCommand } = require('@aws-sdk/client-dynamodb');
+// const { createRedisClient } = require('../helpers/createRedisClient');
 const { postToConnection } = require('../helpers/postToConnection');
 
 module.exports.handler = async (event, context) => {
@@ -9,32 +10,53 @@ module.exports.handler = async (event, context) => {
   // Get connectionId and callbackUrl from event
   const { connectionId, callbackUrl } = event;
 
-  const redisClient = await createRedisClient();
+  // const redisClient = await createRedisClient();
 
   // Depending on the inferences made from user authentication i.e orgId, subscription, we can create a context for the user
   // by pulling the organization's knowledge base from dynamoDB
 
-  // Temporary session context. Should come from dynamoDB
-  const sessionContext = [
-    {
-      role: 'system',
-      content: `MTN has built strong core operations, which are underpinned by the largest fixed and mobile network in Africa; a large, connected registered customer base; an unparalleled registration and distribution network, as well as one of the strongest brands in our markets. John Doe is the CEO of MTN Group. He has been with the Group since April 2017. He is a seasoned executive with a wealth of experience spanning 25 years. He has extensive experience in the telecommunications sector, having held senior leadership roles at Vodafone, Celtel, Safaricom and Vodacom. He has also served on various boards including Vodacom Group and Vodacom South Africa. He is a member of the Board of the GSMA and the Chairman of the MTN GlobalConnect Board. 
-      
-      DO NOT PREFIX YOUR RESPONSE WITH ANY PHRASE. The user does not need to know you're working with a context. Simply respond as appropriate.`,
+  const dynamoDBClient = new DynamoDBClient();
+  
+  // Temporary code to create a knowledge base for MTN
+  const putCommand = new PutItemCommand({
+    TableName: 'project-hilda-dev-orgsTable',
+    Item: {
+      name: { S: 'MTN' },
+      regNo: { S: '1a2b3c' },
+      knowledgeBase: {
+        S: `MTN has built strong core operations, which are underpinned by the largest fixed and mobile network in Africa; a large, connected registered customer base; an unparalleled registration and distribution network, as well as one of the strongest brands in our markets. John Doe is the CEO of MTN Group. He has been with the Group since April 2017. He is a seasoned executive with a wealth of experience spanning 25 years. He has extensive experience in the telecommunications sector, having held senior leadership roles at Vodafone, Celtel, Safaricom and Vodacom. He has also served on various boards including Vodacom Group and Vodacom South Africa. He is a member of the Board of the GSMA and the Chairman of the MTN GlobalConnect Board. DO NOT PREFIX YOUR RESPONSE WITH ANY PHRASE. The user does not need to know you're working with a context. Simply respond as appropriate.`,
+      },
     },
-  ];
+  });
 
-  redisClient
-    .set(connectionId, JSON.stringify(sessionContext))
-    .then(() => {
-      console.log('Session context created');
-    })
-    .catch((e) => {
-      console.log('Error creating session context', e);
-    })
-    .finally(async () => {
-      await redisClient.quit();
-    });
+  await dynamoDBClient.send(putCommand);
+
+  const getCommand = new GetItemCommand({
+    Key: {
+      name: {
+        S: 'MTN',
+      },
+      regNo: {
+        S: '1a2b3c',
+      },
+    },
+    TableName: 'project-hilda-dev-orgsTable',
+  });
+
+  const organization = await dynamoDBClient.send(getCommand);
+  console.log(organization);
+
+  // redisClient
+  //   .set(connectionId, JSON.stringify(sessionContext))
+  //   .then(() => {
+  //     console.log('Session context created');
+  //   })
+  //   .catch((e) => {
+  //     console.log('Error creating session context', e);
+  //   })
+  //   .finally(async () => {
+  //     await redisClient.quit();
+  //   });
 
   //  Send message to client
   const response = {
--- handlers/prompt.js
+++ handlers/prompt.js
@@ -11,15 +11,24 @@ exports.handler = async (event, context) => {
     const connectionId = event.requestContext.connectionId;
     const payload = JSON.parse(event.body);
 
-    console.log('Payload', payload)
+    console.log('Payload', payload);
 
-    const redisClient = await createRedisClient();
+    // const redisClient = await createRedisClient();
 
-    const context = await redisClient.get(connectionId);
+    // const context = await redisClient.get(connectionId);
 
-    const dialog = JSON.parse(context);
+    // const dialog = JSON.parse(context);
 
-    console.log('Session context', dialog);
+    // console.log('Session context', dialog);
+
+    const dialog = [
+      {
+        role: 'system',
+        content: `MTN has built strong core operations, which are underpinned by the largest fixed and mobile network in Africa; a large, connected registered customer base; an unparalleled registration and distribution network, as well as one of the strongest brands in our markets. John Doe is the CEO of MTN Group. He has been with the Group since April 2017. He is a seasoned executive with a wealth of experience spanning 25 years. He has extensive experience in the telecommunications sector, having held senior leadership roles at Vodafone, Celtel, Safaricom and Vodacom. He has also served on various boards including Vodacom Group and Vodacom South Africa. He is a member of the Board of the GSMA and the Chairman of the MTN GlobalConnect Board. 
+        
+        DO NOT PREFIX YOUR RESPONSE WITH ANY PHRASE. The user does not need to know you're working with a context. Simply respond as appropriate.`,
+      },
+    ];
 
     dialog.push({ role: 'user', content: payload.data.message });
 
@@ -40,10 +49,17 @@ exports.handler = async (event, context) => {
     });
 
     const response = await sagemakerClient.send(command);
-    const responseBody = response.Body.toString('utf8');
-    const parsedResponse = JSON.parse(responseBody);
 
-    console.log('Parsed response', parsedResponse)
+    console.log('Response from SageMaker', response);
+
+    // response is Uint8ArrayBlobAdapter(115) [Uint8Array]
+    // convert to string
+
+    const parsedResponse = JSON.parse(
+      new TextDecoder('utf-8').decode(response.Body)
+    );
+
+    console.log('Parsed response', parsedResponse);
 
     const generatedResponse = {
       action: 'prompt',
@@ -56,17 +72,17 @@ exports.handler = async (event, context) => {
 
     console.log('Generated response', generatedResponse);
 
-    redisClient
-      .set(connectionId, JSON.stringify(dialog))
-      .then(() => {
-        console.log('Session context updated');
-      })
-      .catch((e) => {
-        console.log('Error updating session context', e);
-      })
-      .finally(async () => {
-        await redisClient.quit();
-      });
+    // redisClient
+    //   .set(connectionId, JSON.stringify(dialog))
+    //   .then(() => {
+    //     console.log('Session context updated');
+    //   })
+    //   .catch((e) => {
+    //     console.log('Error updating session context', e);
+    //   })
+    //   .finally(async () => {
+    //     await redisClient.quit();
+    //   });
 
     await postToConnection(callbackUrl, connectionId, generatedResponse);
   } catch (error) {
