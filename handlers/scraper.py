6cd1628655462a67f5b4cbbe7568f30422241599
Ebereonwu Einstein Munachiso <einsteinmunachiso@gmail.com>
2023-09-01 13:54:37 +0100

    feat: leadership scraper and questions generator, other minor refactors.

--- handlers/scraper.py
+++ handlers/scraper.py
@@ -1,4 +1,5 @@
 import requests
+import jsonlines
 from colorama import Fore
 from bs4 import BeautifulSoup
 from colorama import init as colorama_init
@@ -50,6 +51,10 @@ ALL_URLS = {
         "router_plans" : "https://www.mtn.ng/helppersonal/router-plans/",
         "xtravalue_more" : "https://www.mtn.ng/helppersonal/xtravalue-more/",
         "startimes_on" : "https://www.mtn.ng/helppersonal/startimes-on/"
+    },
+    "leadership":
+    {
+        "executive_bod" : "https://www.mtn.ng/leadership/?tablink=executive", # Info about executives and BOD can be found here.
     }
 }
 
@@ -57,18 +62,19 @@ ALL_URLS = {
 # Error message in event of empty responses.
 error_message = "URLIssue: Please check that you provided the right URL to the function.\nIf the issue persiste then it means MTN has made changes to their website structure."
 
-def get_bundle_type_info(url:str) -> str:
+def get_bundle_type_info(url:str, filename) -> str:
     """
     This function scrapes all data regarding mobile bundles on the MTN website and returns them as a string.   
 
     Input:
         url:str => URL for MTN Mobile Plans page (Under Data).
+        filename:str => Filename to store the JSONL dataset as. snake_case.jsonl naming convention is encouraged.
     Output:
-        mobile_data_info:str => String containing all info of all mobile plan options including bundle, price and how to purchase
+        JSONL file containing questions and answers.
     """
 
     
-    data_info = """""" # String containing combination of all data bundles info
+    data_info = [] # list containing questions and answers to be stored in JSONL file format.
 
     try:
         response = requests.get(url)
@@ -84,13 +90,40 @@ def get_bundle_type_info(url:str) -> str:
             for info in other_info:
                 other_info_combined += info.text.strip()+" "
 
-            data_info += f"{plan} cost {price}, {other_info_combined.strip()}. "
-
-            # Raising exception in event of empty string.
-            if len(data_plan_cards) == 0:
-                raise Exception(error_message)
+            # Generating questions.
+            question_1 = f"I want to buy {plan} of data"
+            question_2 = f"How much is the {plan}?"
+            question_3 = f"How do I purchase {plan}"
+            question_4 = f"To purchase the {plan}, what do I do?"
+            question_5 = f"Does MTN have {plan}"
+            question_6 = f"Can I buy {plan}?"
+            
+            # Handling missing purchase info
+            if len(other_info_combined.split()) != 4:
+                other_info_combined = (other_info_combined.strip().replace('"', "").replace("‘", "").replace("’", "")
+                .replace("(available on myMTN NG app)", "kindly check our MTN app, it is").replace("Hrs", "hours").replace("hr", " hour(s)"))
             else:
-                return data_info
+                other_info_combined = "Dial *312#"
+
+            # Generating questions.
+            response_1 = f"Alright, to purchase {plan}, at {price}, {other_info_combined}."
+            response_2 = f"{plan} cost {price}, {other_info_combined}."
+            response_3 = f"To purchase {plan} at {price}, {other_info_combined}."
+            response_4 = f"To purchase {plan} which cost {price}, {other_info_combined}."
+            response_5 = f"MTN has {plan} seling as {price}, {other_info_combined}."
+            response_6 = f"To buy {plan}, {other_info_combined}."
+            
+            # Generating responses.
+            data_info.append({"prompt" : f"question: {question_1}", "completion" : response_1})
+            data_info.append({"prompt" : f"question: {question_2}", "completion" : response_2})
+            data_info.append({"prompt" : f"question: {question_3}", "completion" : response_3})
+            data_info.append({"prompt" : f"question: {question_4}", "completion" : response_4})
+            data_info.append({"prompt" : f"question: {question_5}", "completion" : response_5})
+            data_info.append({"prompt" : f"question: {question_6}", "completion" : response_6})
+
+        # Writing list of questiona snd answers to JSONL file.   
+        with jsonlines.open(filename, 'w') as writer:
+            writer.write_all(data_info)
 
     except Exception as e:
         print(f"{Fore.RED}Error encountered with get_bundle_type_info(): \n{Fore.MAGENTA}{e}")
@@ -99,6 +132,7 @@ def get_bundle_type_info(url:str) -> str:
 def data_gifting_info(url:str) -> str:
     """
     This function scrapes all data (FAQs excluded) on the data gifting page
+
     Input:
         url:str => URL for MTN Data Gifting page (Under Data).
     Output:
@@ -125,39 +159,124 @@ def data_gifting_info(url:str) -> str:
         print(f"{Fore.RED}Error encountered with data_gifting_info(): \n{Fore.MAGENTA}{e}")
 
 
-def get_FAQs(url:str) -> (list, list):
+def get_FAQs(url:str, filename:str):
     """
     This function takes in the URL to the particular FAQs page and returns a tule containing   
     two lists One containing all FAQs and the other containing all responses to the FAQs.
+    
     Input:
         url:str => URL for MTN Data Gifting page (Under Data).
+        filename:str => Filename to store the JSONL dataset as. snake_case.jsonl naming convention is encouraged.
     Output:
-        FAQs_list:list => List containing all FAQs for service link provided.
-        FAQs_answers:list => List containing all responses to FAQs.
+        JSONL file containing questions and answers.
     """
     try:
+        FAQs_json_list = [] # list containing questions and answers to be stored in JSONL file format.
         response = requests.get(url)
         soup = BeautifulSoup(response.content, features="lxml")
-
-        FAQs_list = [] # List of FAQs under topic link provided.
-        FAQs_answers = [] # List of answers for each FAQ.
 
         FAQs = soup.find_all("span", {"class" : "accordion-title"})
         responses = soup.find_all("div", {"class" : "panel"})
 
         for question, response in zip(FAQs, responses):
-            FAQs_list.append(question.text.strip())
-            FAQs_answers.append(response.text.strip())
+            FAQs_json_list.append({"prompt" : f"question: {question.text.strip()}", "completion" : response.text.strip()[3:].replace("\n", " ").replace("  ", " ")})
 
         # Raise exception if lists are empty, meaning URL has an issue.
-        if len(FAQs_list) | len(FAQs_answers) == 0:
+        if len(FAQs_json_list) == 0:
             raise Exception(error_message)
         else:
-            return(FAQs_list, FAQs_answers)
+            # Writing list of questiona snd answers to JSONL file.   
+            with jsonlines.open(filename, 'w') as writer:
+                writer.write_all(FAQs_json_list)
+
         
     except Exception as e:
         print(f"{Fore.RED}Error encountered with get_FAQs(): \n{Fore.MAGENTA}{e}")
 
 
-response = data_gifting_info("https://www.google.com")
-print(response)
+def get_executive_committee(url:str, filename):
+    """
+    This function gets details about each member of the executive arm of MTN
+    Input:
+        url:str => URL for MTN Mobile Plans page (Under Data).
+        filename:str => Filename to store the JSONL dataset as. snake_case.jsonl naming convention is encouraged.
+    Output:
+        returns jsonl file containing questions and answers regarding executives in MTN.
+    """
+    
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, features="lxml")
+
+    position_name_dict = {} # Dictionary containing names and positions
+    names = [name.text for name in soup.find_all("h5")] # Extracting names of officers from the MTN website
+    positions = [position.text for position in soup.find_all("div", {"class" : "leader-designation"})] # Extracting offices/positions.
+    executive_committee_info = [] # list containing questions and answers to be stored in JSONL file format.
+
+    # Checking for multiple individuals with same role and formating data accordingly
+    for name, position in zip(names, positions):
+        if position_name_dict.get(position) == None: # Checking if the role exists in the dataset already
+            position_name_dict[position] = [name] # Creating list of names for a particular position if it doesn't exist
+        else:
+            position_name_dict[position].append(name) # Adding new name to list of already existing names for that position. 
+
+
+    # Generating questions responses bearing in mind that certain positions have multiple individuals. 
+    for position in position_name_dict.keys():
+        if len(position_name_dict[position]) == 1: # Position held by only one person.
+            name = position_name_dict[position][0] # Extracting name of officer from dictionary
+            # Generating questions.
+            question_1 = f"Who is the {position} of MTN?"
+            question_2 = f"MTNs {position} is called?"
+            question_3 = f"What is the name of MTNs {position}"
+
+            # Generating responses to questions.
+            response_1 = f"The current {position} of MTN is {name}"
+            response_2 = f"The current {position} of MTN is called {name}"
+            response_3 = f"The name of the current {position} of MTN is {name}"
+
+            # Generating JSON data template and appending to list of Q&A
+            executive_committee_info.append({"prompt" : f"question: {question_1}", "completion" : response_1})
+            executive_committee_info.append({"prompt" : f"question: {question_2}", "completion" : response_2})
+            executive_committee_info.append({"prompt" : f"question: {question_3}", "completion" : response_3})
+        else: # Handling positions that have multiple individuals.
+            position_names = ", ".join(position_name_dict[position]) # Converting individual names into a string
+            num_in_position = len(position_name_dict[position]) # Number of people currently holding the position.
+
+            # Generating questions.
+            question_1 = f"Who is the {position} of MTN?"
+            question_2 = f"MTNs {position} is called?"
+            question_3 = f"What is the name of MTNs {position}"
+            question_4 = f"Who are the {position}s of MTN?"
+            question_5 = f"MTNs {position}s are called?"
+            question_6 = f"What are the name of MTNs {position}s"
+
+            # Generating responses to questions.
+            response_1 = f"The current {position}s of MTN are {position_names}"
+            response_2 = f"Current, there are {num_in_position} {position}s {position_names}"
+            response_3 = f"The names of the current {position}s of MTN are {position_names}"
+            response_4 = f"The current {position}s of MTN are {position_names}"
+            response_5 = f"Current, there are {num_in_position} {position}s {position_names}"
+            response_6 = f"The names of the {num_in_position} {position}s of MTN are {position_names}"
+
+            # Generating JSON data template and appending to list of Q&A
+            executive_committee_info.append({"prompt" : f"question: {question_1}", "completion" : response_1})
+            executive_committee_info.append({"prompt" : f"question: {question_2}", "completion" : response_2})
+            executive_committee_info.append({"prompt" : f"question: {question_3}", "completion" : response_3})
+            executive_committee_info.append({"prompt" : f"question: {question_4}", "completion" : response_4})
+            executive_committee_info.append({"prompt" : f"question: {question_5}", "completion" : response_5})
+            executive_committee_info.append({"prompt" : f"question: {question_6}", "completion" : response_6})
+
+        # Writing list of questiona snd answers to JSONL file.   
+        with jsonlines.open(filename, 'w') as writer:
+            writer.write_all(executive_committee_info)
+
+    if len(names) | len(positions) == 0: # Checking if any data was succesfully scraped.
+        print(f"{Fore.RED}Error encountered with get_executive_committee():\n{Fore.MAGENTA}Check that the url provided is correct, if error persists then MTN has made changes to their website")
+    else:
+        # Confirmation message.
+        print(f"{Fore.LIGHTGREEN_EX}Data saved as .jsonl in '{filename}'")
+
+
+get_executive_committee(ALL_URLS["leadership"]["executive_bod"])
+# response = data_gifting_info("https://www.google.com")
+# print(response)
