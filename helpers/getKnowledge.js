8e997e303ceba55c0d7ea6acb205fa692b02b927
ctrl-m-q <64306905+ctrl-m-q@users.noreply.github.com>
2023-11-04 14:35:17 +0100

    Update getKnowledge.js

--- helpers/getKnowledge.js
+++ helpers/getKnowledge.js
@@ -1,16 +1,25 @@
 module.exports.getKnowledge = async (agentName) => {
-  const base = ''
-  if (agentName === 'Hilda') {
-    return '';
-  }
+    const base = ''
+    if (agentName === 'Hilda') {
+      return '';
+    }
+  
+    if (agentName === 'Ajoke') {
+      return '';
+    }
+  
+    if (agentName === 'Ayeesha') {
+        console.log('Hello! I am Ayeesha! Here to help you with what you may need today ');
+      if (userQuery.toLowerCase() == "What is your name? "){
+        return "My name is Ayeesha.";
 
-  if (agentName === 'Ajoke') {
+      } else if (userQuery.toLowerCase().includes(" ")){
+        return " ";
+      }
+    }
+    else {
+        return "I am sorry, I do not have that specific information at the moment";
+    }
+  
     return '';
-  }
-
-  if (agentName === 'Ayeesha') {
-    return '';
-  }
-
-  return '';
-};
+  };
